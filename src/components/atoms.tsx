9916bd5b1501650f34488e968ac1c61b0b2c06d2
sdfwefk11 <mknhsad3398@naver.com>
2023-04-22 20:20:10 +0900

    Selector2

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -5,7 +5,10 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
   id: number;
 }
-
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
@@ -14,10 +17,7 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
